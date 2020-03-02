6b0be49a2c9216444b2501c04978133c60e75a58
Mark Tomm <marktomm90@gmail.com>
2020-03-03 01:38:23 +0200

    fix docker-compose download

--- run.sh
+++ run.sh
@@ -23,7 +23,7 @@ sudo apt-get update
 
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
-sudo curl -L "https://github.com/docker/compose/releases/download/$(docker -v | sed 's/.*version \([0-9.]*\),.*/\1/')/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
 
